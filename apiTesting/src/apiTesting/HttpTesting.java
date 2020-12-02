84c7cb25b0c24216ec630b481288a3ef402a3aee
seenu <seenu@DESKTOP-25U6JPU>
2020-12-02 06:54:38 -0800

    First commit

---
+++ apiTesting/src/apiTesting/HttpTesting.java
@@ -0,0 +1,16 @@
+package apiTesting;
+
+import java.net.URI;
+import java.net.URL;
+
+public class HttpTesting {
+	
+	URL url=new URI("");
+	url.get
+	
+
+	public static void main(String[] args) {
+
+	}
+
+}
